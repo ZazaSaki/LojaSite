58569262db6a51534cc27f1bc07c6b1903a6c62c
Zaza Saki <isac.cruzn@gmail.com>
2021-03-26 16:53:47 +0000

    card component criation

---
+++ components/Card_1.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Media } from 'react-bootstrap';
+import {card} from "../styles/extra.module.css";
+
+
+export default function Card({text, title, src}){
+    return (
+        <Media className = {card}>
+            <img
+            width={250}
+            height={300}
+            className="mr-3"
+            src={src}
+            alt="Generic placeholder"
+            />
+            <Media.Body>
+            <h5>{title}</h5>
+            <p>
+                {text}
+            </p>
+        
+            
+            </Media.Body>
+        </Media>
+      );
+}
--- pages/test.js
+++ pages/test.js
@@ -1,11 +1,24 @@
 
+import CardGroup from "../components/CardGroup";
+import Card from "../components/Card_1";
 import FirtsPageCarousel from "../components/FirstPageCarousel";
 import TopBar from "../components/TopBar";
 
+const CardList = [
+    {title : "Apresentação 1",src : "/250x300.svg",  text : "Texto Publicitario aleatorio 1"},
+    {title : "Apresentação 2",src : "/250x300.svg",  text : "Texto Publicitario aleatorio 2"},
+    {title : "Apresentação 3",src : "/250x300.svg",  text : "Texto Publicitario aleatorio 3"}
+]
+
+const card = CardList[0];
+
+//<CardGroup List = {CardList}></CardGroup>
 
 export default function test(){
     return(<div>
         <TopBar></TopBar>
         <FirtsPageCarousel></FirtsPageCarousel>
+        <CardGroup List = {CardList}></CardGroup>
+        
     </div>);
 }
