1f50cd4be0f8d84ed3157174631223dec6c4f4ee
Zaza Saki <isac.cruzn@gmail.com>
2021-03-31 09:22:25 +0100

    card to show categories

---
+++ components/Card_Type.js
@@ -0,0 +1,18 @@
+import { Button, Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Card_2({src, text, title, link}) {
+    return(
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={src} style={{width : '100%', height : '250px'}}/>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                {text}
+                </Card.Text>
+                <Button variant="primary">ver</Button>
+                
+            </Card.Body>
+        </Card>
+    );
+}
