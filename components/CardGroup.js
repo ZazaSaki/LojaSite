5022bd2323c32e21559e3c0b330601d7927a3767
Zaza Saki <isac.cruzn@gmail.com>
2021-03-31 09:23:08 +0100

    cards orientation

--- components/CardGroup.js
+++ components/CardGroup.js
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function CardGroup({List, Card}){
     return (
-        <div>
+        <div class='row'>
             {List.map(card => (
-                <Card title = {card.title} text = {card.text} src = {card.src} key = {card.title}/>
+                <Card title = {card.title} text = {card.text} src = {card.src} link={card.link} key = {card.title}/>
                 
             ))}
         </div>
