d9f2cc53f18ce48479caf59fb75c3a07616f605c
Zaza Saki <isac.cruzn@gmail.com>
2021-04-02 12:42:15 +0100

    create : search Item and search list

---
+++ components/SearchItem.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';  
+
+import { ListGroupItem } from "react-bootstrap";
+
+
+export default function SearchItem({product, selfAdd}){
+    
+    
+    return <ListGroupItem action onClick={()=>{selfAdd(product);}}>{product.name}</ListGroupItem>
+}
---
+++ components/seachList/SearchList.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import {search as searchClass} from "../../styles/etiqueta.module.css";
+import { ListGroup } from "react-bootstrap";
+import SearchItem from '../SearchItem';
+
+
+export default function SearchList({list, action, active}){
+    
+    const dd = active? {display: 'block'} : {display: 'none'};
+    return (
+        
+                <ListGroup className={searchClass} style={dd}>
+                    {list.map((product)=>(<SearchItem product={product} selfAdd={action}></SearchItem>))}
+                </ListGroup>
+        
+        );
+}
