cfc64b07085ab81a4435c6f47689cc98d22f8e72
Zaza Saki <isac.cruzn@gmail.com>
2021-04-02 08:52:40 +0100

    getList an getTitle functions, to fix no list bugs

--- components/Etiqueta.js
+++ components/Etiqueta.js
@@ -1,7 +1,28 @@
 import {quadro, logo, name, price, table, td, h3} from "../styles/etiqueta.module.css";
 
 
-export default function Etiqueta({List}){
+
+export default function Etiqueta({List, title}){
+
+    function getTitle(){
+        if (!title) {
+            if(!List){
+                return " ";
+            }
+
+            return List[0].family;
+        }
+
+        return title;
+    }
+
+    function getList(){
+        if (!List) {
+            return [];
+        }
+
+        return List;
+    }
 
     return(
         <div className = {quadro}>
@@ -12,13 +33,13 @@ export default function Etiqueta({List}){
             <img className={logo} src = "https://scontent.flis4-1.fna.fbcdn.net/v/t1.18169-9/15350713_446076315516114_6277619616950754977_n.png?_nc_cat=107&ccb=1-3&_nc_sid=973b4a&_nc_ohc=yUDyQuhX1nMAX9nVZDN&_nc_ht=scontent.flis4-1.fna&oh=ea9f041d6d22821d574b6e4203722b11&oe=608C248B"/>
             </th>
             <th className = {td}>
-            <h3 className = {h3}>{List[0].family}</h3>
+            <h3 className = {h3}>{getTitle()}</h3>
             </th>
             </tr>
             </table>
         <table className = {table}>
             
-            {List.map(product => (
+            {getList().map(product => (
                 <tr>
                     <td className = {name}>{product.name}</td>
                     <td className = {price}>{product.price}</td>
