77562017d96b9737d0671e050738610da4e5d330
Zaza Saki <isac.cruzn@gmail.com>
2021-04-10 15:32:05 +0100

    hello last fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,15 +2,24 @@
 //import { readExcel } from "../../src/execelRd";
 //import jsWritter from "../../src/fileJsWriter";
 import fs from 'fs';
-
+import path from 'path';
 
 
 export default (req, res) => {
   //const List = readExcel('./public/exelTest.xlsx');
-  
+  const dirRelativeToPublicFolder = 'dataBase.json';
+
+  const dir = path.resolve('./public', dirRelativeToPublicFolder);
   const data = fs.readFileSync('./public/dataBase.json');
 
-  const {List} = JSON.parse(data); 
+  const {List: rawList} = JSON.parse(data);
+  
+  const List = rawList.map(({id, name, family,price})=>(
+    {id,
+    family,
+    name : name.replace(family, '').trim(),
+    price}
+  ));
 
   res.statusCode = 200
   res.json(List);
