df4ae0440385d5cfec0a29f2b22147e7c3ee1f9c
Zaza Saki <isac.cruzn@gmail.com>
2021-04-10 11:28:26 +0100

    hello bug fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { readExcel } from "../../src/execelRd";
-import jsWritter from "../../src/fileJsWriter";
+//import { readExcel } from "../../src/execelRd";
+//import jsWritter from "../../src/fileJsWriter";
+import fs from 'fs';
 
 
 
 export default (req, res) => {
-  const List = readExcel('./public/exelTest.xlsx');
-  jsWritter(List);
+  //const List = readExcel('./public/exelTest.xlsx');
+  
+  const data = fs.readFileSync('./public/dataBase.json');
+
+  const {List} = JSON.parse(data); 
 
   res.statusCode = 200
   res.json(List);
+
+  return;
 }
