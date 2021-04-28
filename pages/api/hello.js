2007b693149b19929c6f586f9cff071b1d7e89a7
Zaza Saki <isac.cruzn@gmail.com>
2021-04-28 14:11:24 +0100

    Last try

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import {readExcel } from "../../src/execelRd";
-//import jsWritter from "../../src/fileJsWriter";
-
 import fs from "fs";
 import path from "path";
+import xlsx from 'xlsx';
+//import jsWritter from "../../src/fileJsWriter";
+
 function jsWritter(List) {
     //const images = filenames.map(name => path.join('/', dirRelativeToPublicFolder, name))
     const data = JSON.stringify({
@@ -13,6 +13,32 @@ function jsWritter(List) {
 
     fs.writeFileSync("./public/dataBase.json", data);
 
+}
+
+function readExcel() {
+  const dirRelativeToPublicFolder = 'exelTest.xlsx';
+  const dir = path.resolve('./public', dirRelativeToPublicFolder);
+
+  const wb = xlsx.readFile(dir);
+  const ws = wb.Sheets['Produtos'];
+  let List = []
+  const cell1 = ws['L10'];
+  const cell2 = ws['A10'];
+  let i=5;
+  
+  function cell(letter){
+
+      if(ws[letter+i]) return ws[letter+i].v;
+      return null
+  }
+
+  do {
+      
+      List.push({id:cell('A'), name: cell('C'), family : cell('B'), price : cell('L').toFixed(2)+"€"});
+      i = i+1;
+  } while (cell('A'));
+
+  return List;
 }
 
 export default (req, res) => {
