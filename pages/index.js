2059533ad0e9173f73c816890e343028c59692d3
Zaza Saki <isac.cruzn@gmail.com>
2023-09-08 18:06:24 +0100

    implementing MyCard on index

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,11 @@ import TopBar from "../components/TopBar";
 const trashtext = "'Tis better to be vile than vile esteem'd, When not to be receives reproach of being; And the just pleasure lost, which is so deem'd Not by our feeling, but by others' seeing: For why should others' false adulterate eyes Give salutation to my sportive blood? Or on my frailties why are frailer spies, Which in their wills count bad what I think good? No, I am that I am, and they that level At my abuses reckon up their own:";
 
 const CardList = [
-    {title : "Apresentação 1",src : "/250x300.svg",  text : trashtext},
+    {title : "Apresentação 1",src : "/background.jpg",  text : trashtext},
     {title : "Apresentação 2",src : "/250x300.svg",  text : trashtext},
-    {title : "Apresentação 3",src : "/250x300.svg",  text : trashtext}
+    {title : "Apresentação 3",src : "/map.png",  text : trashtext},
+    {title : "Apresentação 4",src : "/250x300.svg",  text : trashtext},
+    {title : "Apresentação 5",src : "/250x300.svg",  text : trashtext}
 ]
 
 const card = CardList[0];
@@ -20,19 +22,16 @@ export default function Home(){
     return(<div>
         <TopBar></TopBar>
         <FirtsPageCarousel></FirtsPageCarousel>
-        <MyCard 
-            img_src={"/logo.png"} 
-            title = "Cartão de teste" 
-            text={"Vou escrever mesmo bue aqui"} 
-            flipped = {false}>
-        </MyCard>
-        <MyCard 
-            img_src={"/logo.png"} 
-            title = "Cartão de teste" 
-            text={"Vou escrever mesmo bue aqui"} 
-            flipped = {true}>
-        </MyCard>
-        <CardGroup List = {CardList} Card = {Card}></CardGroup>
+
+        {CardList.map((card,index) => (
+                <MyCard 
+                    title={card.title}
+                    text={card.text}
+                    src={card.src}
+                    flipped = {index%2 == 0}
+                    key = {index}
+                />
+            ))}
         
     </div>);
 }
